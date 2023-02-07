f487b884fd05a759a8a1752b6efba77c5235b087
Yuni <newnny0812@gmail.com>
2023-02-07 22:49:20 +0100

    add main image

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { useMediaQuery, useTheme } from "@mui/material"
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
 import './Page.css'
+import mainImage from '../Images/YUNI_Z.png'
 
-const Main: React.FC  = () => {
+const Main: React.FC = () => {
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.down('sm'));
     return (
         <div className="App-page">
             <div className={matches ? 'main-mobile-div' : 'main-div'}>
+            <img src={mainImage} alt="MainImage" style={{maxWidth: 500}} />
                 <p className='main-text'>
                     <KeyboardArrowRightIcon />
                     Hello,
