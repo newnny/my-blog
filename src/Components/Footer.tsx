3fcd493f0d664a8607b5e4216da82b53813c70a6
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2024-05-02 11:16:08 +0200

    fix(footer): typo

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,7 +3,7 @@ import '../App.css'
 const Footer = () => {
   return (
     <div className='footer-div'>
-      get it touch&nbsp;
+      get in touch&nbsp;
       <a className="a" style={{ color: "grey" }} href="mailto:newnny0812@gmail.com" target="_blank" rel="noreferrer">email</a>&nbsp;
       |&nbsp;<a className="a" style={{ color: "grey" }} href="https://www.linkedin.com/in/newnny/" target="_blank" rel="noreferrer">linkedin</a>&nbsp;
       |&nbsp;<a className="a" style={{ color: "grey" }} href="https://github.com/newnny" target="_blank" rel="noreferrer">github</a>
